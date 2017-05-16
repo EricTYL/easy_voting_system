810e01ec7cc886be2a1c75324a711c9250072b14
RickyTYL <rickytyl@icloud.com>
2017-05-16 11:15:11 +0800

    finish add candidate feature

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,4 +2,24 @@ class CandidatesController < ApplicationController
   def index
     @candids = Candidate.all
   end
+
+  def new
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      redirect_to candidates_path, notice: "add candidate successfully"
+    else
+      render :new
+    end
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:age, :name, :politics, :party)
+  end
+
 end
