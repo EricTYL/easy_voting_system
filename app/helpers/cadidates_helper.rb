23b18026ce13a24912d0c9573f15481e8ab94731
RickyTYL <rickytyl@icloud.com>
2017-05-16 10:05:54 +0800

    generate candids controller & model

---
+++ app/helpers/cadidates_helper.rb
@@ -0,0 +1,2 @@
+module CadidatesHelper
+end
---
+++ test/controllers/cadidates_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CadidatesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
