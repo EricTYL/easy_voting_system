2bfa5dbe7050158d5153867737c3fc8c269d5c83
RickyTYL <rickytyl@icloud.com>
2017-05-16 15:54:40 +0800

    reduce same code by before_action

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,7 @@
 class CandidatesController < ApplicationController
+
+  before_action :find_candidate, only: [:edit, :update, :destroy, :vote]
+
   def index
     @candids = Candidate.all
   end
@@ -18,12 +21,9 @@ class CandidatesController < ApplicationController
   end
 
   def edit
-    @candidate = Candidate.find_by(id: params[:id])
   end
 
   def update
-    @candidate = Candidate.find_by(id: params[:id])
-
     if @candidate.update(candidate_params)
       redirect_to candidates_path, notice: "edit candidate successfully"
     else
@@ -32,13 +32,11 @@ class CandidatesController < ApplicationController
   end
 
   def destroy
-    @candidate = Candidate.find_by(id: params[:id])
     @candidate.destroy if @candidate
     redirect_to candidates_path, notice: "delete candidate successfully"
   end
 
   def vote
-    @candidate = Candidate.find_by(id: params[:id])
     @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate
     #    render :index 
     # it doesn't work because there's no candids variable here, error appear when render index.html.erb
@@ -50,4 +48,7 @@ class CandidatesController < ApplicationController
     params.require(:candidate).permit(:age, :name, :politics, :party)
   end
 
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[:id])
+  end
 end
